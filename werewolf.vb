a23e6fe8773b22a16f7cb324e6140f3a4a4459ba
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-10-08 16:37:05 +0200

    Added using names as player identification

--- werewolf.vb
+++ werewolf.vb
@@ -8,17 +8,46 @@ Module werewolf
     
     Sub Main(args As String())
         If args.Length = 0 Then
-            Console.Write("Enter amount of players: ")
-            Start(Console.Readline())
+            Console.Write("Use Player nAmes or Numbers? (p/a/n/#): ")
+            tmpString = Console.ReadKey().Key.ToString
+            Console.Writeline()
+            If tmpString = "P" Or tmpString = "A"
+                Console.WriteLine("Enter player names. Type ""done"" when done.")
+                tmpString = Console.ReadLine()
+                Do Until tmpString = "done"
+                    playerNames.Add(tmpString)
+                    tmpString = Console.ReadLine()
+                Loop
+                Start(playerNames.Count, False)
+            ElseIf tmpString = "N" Or tmpString = "0" ' # relates to 0 for some reason. So does \ and probably a few other characters but that's fine.
+                Console.Write("Enter amount of players: ")
+                Start(Console.Readline())
+            Else
+                Console.Writeline("""" & tmpString & """ isn't any of the valid inputs!")
+            End If
         Else
             Select case args(0)
                 Case "-h"
                     Console.Writeline("Werewolf - github.com/Walkman100/Werewolf")
                     WriteUsage()
-                Case "-p"
+                Case "-n"
                     If args.length > 1 Then : Start(args(1))
                     Else : Console.Write("Enter amount of players: ")
                            Start(Console.Readline())
+                    End If
+                Case "-p"
+                    If args.length > 1 Then : 
+                        For Each name In args(1).Split(",")
+                            playerNames.Add(name)
+                        Next
+                        Start(playerNames.Count, False)
+                    Else : Console.WriteLine("Enter player names. Type ""done"" when done.")
+                           tmpString = Console.ReadLine()
+                           Do Until tmpString = "done"
+                               playerNames.Add(tmpString)
+                               tmpString = Console.ReadLine()
+                           Loop
+                           Start(playerNames.Count, False)
                     End If
                 Case Else
                     Console.Writeline("Unrecognised flag """ & args(0) & """!")
@@ -28,7 +57,7 @@ Module werewolf
     End Sub
 
     Sub WriteUsage()
-        Dim flags As String = " [-h|-p [number of players]]"
+        Dim flags As String = " [-h|-n [number of players]|-p [playernames seperated by ,]]"
         Dim programPath As String = System.Reflection.Assembly.GetExecutingAssembly().CodeBase
         If My.Computer.Info.OSPlatform = "Unix" Then
             Console.Writeline("Usage: mono " & programPath.Substring(programPath.LastIndexOf("/") +1) & flags)
@@ -40,21 +69,23 @@ Module werewolf
         End If
     End Sub
     
-    Sub Start(players As String)
-        If isnumeric(players)
-            playerNames.Clear()
-            For i = 1 to players
-                playerNames.Add(i)
-            Next
-            
-            j = 0
-            For Each i In GenerateRandomList(players)
-                Console.WriteLine(playerNames(j) & ": " & i)
-                j += 1
-            Next
-        Else
-            Console.WriteLine("""" & players & """ is not a number!")
+    Sub Start(players As String, Optional generatePlayers As Boolean = True)
+        If generatePlayers
+            If isnumeric(players)
+                playerNames.Clear()
+                For i = 1 to players
+                    playerNames.Add(i)
+                Next
+            Else
+                Console.WriteLine("""" & players & """ is not a number!")
+            End If
         End If
+        
+        j = 0
+        For Each i In GenerateRandomList(players)
+            Console.WriteLine(playerNames(j) & ": " & i)
+            j += 1
+        Next
     End Sub
     
     ''' <summary>Generates a list of unique random numbers</summary>
